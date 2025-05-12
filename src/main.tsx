88b0d3e3ec16666339907ff6eb92c718a27ce592
Pranay <sharmapranay38@gmail.com>
2025-05-12 21:29:51 +0530

    saved main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import appRouter from "./router";
 import "./index.css";
-import { setCookie } from "./utils/cookies";
-setCookie(
-  "auth_token",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1OTJhMzQxMi0zOTM3LTQwNjMtODFkMi0zZDZhYzVhNjUxYTMiLCJlbWFpbCI6InB1c2hrYXIxNzEzQGdtYWlsLmNvbSIsImlhdCI6MTc0NzA2NTE4NiwiZXhwIjoxNzQ3MTAxMTg2fQ.W5ynOym7OTDlHApRayG9k6Mcbomq8L0MLbU8c6XzuNU"
-);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
