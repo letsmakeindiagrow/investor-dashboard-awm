4645d169a34c4464436570ffce670acf4c49846d
Pranay <sharmapranay38@gmail.com>
2025-05-12 21:28:05 +0530

    added my portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { setCookie } from "./utils/cookies";
 setCookie(
   "auth_token",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1OTJhMzQxMi0zOTM3LTQwNjMtODFkMi0zZDZhYzVhNjUxYTMiLCJlbWFpbCI6InB1c2hrYXIxNzEzQGdtYWlsLmNvbSIsImlhdCI6MTc0Njg0OTYzNCwiZXhwIjoxNzQ2ODg1NjM0fQ.gaIUa3r3LlESAi9nCWutzsH7W_VB70_ds6bwUnwmu70"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1OTJhMzQxMi0zOTM3LTQwNjMtODFkMi0zZDZhYzVhNjUxYTMiLCJlbWFpbCI6InB1c2hrYXIxNzEzQGdtYWlsLmNvbSIsImlhdCI6MTc0NzA2NTE4NiwiZXhwIjoxNzQ3MTAxMTg2fQ.W5ynOym7OTDlHApRayG9k6Mcbomq8L0MLbU8c6XzuNU"
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
