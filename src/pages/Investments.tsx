a107c8c8f71c02d48e5acf1c27620c71c8b4dc4d
Pranay <sharmapranay38@gmail.com>
2025-06-08 22:28:58 +0530

    added more info

--- src/pages/Investments.tsx
+++ src/pages/Investments.tsx
@@ -821,7 +821,7 @@ const Investments: React.FC = () => {
                       className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
                       onClick={() => setSelectedInvestment(item)}
                     >
-                      More Info
+                      View Fund
                     </button>
                   </td>
                 </tr>
